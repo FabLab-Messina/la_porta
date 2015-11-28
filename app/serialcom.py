7a3a8e470695b9634520b2b89eaa3399307ea2b5
Francesco Franchina <cescus92@gmail.com>
2015-11-28 18:48:08 +0100

    Rimossa sporcizia e corretti i check sulla disponibilità della seriale

--- app/serialcom.py
+++ app/serialcom.py
@@ -15,7 +15,7 @@ class SerialCom:
     def __init__(self, event_manager, port='/dev/ttyS0', baud=9600):
 
         self._event_manager = event_manager
-        self._event_manager.register_event('on_porta_unlock')
+        #self._event_manager.register_event('on_porta_unlock') TODO
 
         self._port = port
         self._baud = baud
@@ -30,24 +30,25 @@ class SerialCom:
     def send_packet(self, target, *, action=None, payload=None):
         if action == None and payload == None:
             raise Exception('Need to specify an action or a payload for the given target.')
-
-        if not self._busy: self._busy = True # quick and dirty
+        # if an action is specified will override the payload usage
         
         if target in targets:
             if action != None and action in targets[target]['actions']:
+                if not self._busy: self._busy = True # quick and dirty
+
                 packet = b'\xAA'
                 packet += bytes( [ targets[target]['id'] ] )
                 packet += bytes( [ 1, targets[target]['actions'][action] ] )
                 packet += b'\xBB\xCC\xDD'
-                print('ciao')
+
                 self._serial.write(packet)
-                print('packet')
-                
+
+                self._busy = False
                 return True
                 
-
             elif payload != None and action in targets[target]:
                 pass
+                
                 #return True
 
             else:
