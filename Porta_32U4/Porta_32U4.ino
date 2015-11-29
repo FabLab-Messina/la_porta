ae3c400570b9bffd6a5674a9077416c3b6ea2b02
Francesco Franchina <cescus92@gmail.com>
2015-11-29 01:12:59 +0100

    Adattato il codice per Arduino Yùn

--- Porta_32U4/Porta_32U4.ino
+++ Porta_32U4/Porta_32U4.ino
@@ -1,6 +1,7 @@
 //|  0xAA  |  FUN  |  N_PARA   | PARA_1  | PARA_2  | ...  |  PARA_N  |  0xBB, 0xCC, 0xDD  |
 
-#define N_AZIONI 3
+#define N_AZIONI 1
+#define PIN 13
 
 typedef struct azione {
   unsigned char command;
@@ -12,21 +13,13 @@ azione azioni[N_AZIONI];
 
 void fun1()
 {
-  digitalWrite(13, HIGH);
+  digitalWrite(PIN, HIGH);
+  delay(500);
+  digitalWrite(PIN, LOW);
+  
 }
 
-void fun2()
-{
-  digitalWrite(13, LOW);
-}
 
-void fun3()
-{
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
-}
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -35,26 +28,19 @@ void setup() {
   azioni[0].nPara = 1;
   azioni[0].action = fun1;
 
-  azioni[1].command = 0x02;
-  azioni[1].nPara = 1;
-  azioni[1].action = fun2;
 
-  azioni[2].command = 0x03;
-  azioni[2].nPara = 1;
-  azioni[2].action = fun3;
+  Serial1.begin(115200);
 
-  Serial.begin(9600);
-
-  Serial.println("Buongiorno!");
+  Serial1.println("Buongiorno!");
 
 }
 
 void loop() {
 
-  if (Serial.available() && Serial.read() == 0xAA)
+  if (Serial1.available() && Serial1.read() == 0xAA)
   {
-    byte command = Serial.read();
-    byte nPara = Serial.read();
+    byte command = Serial1.read();
+    byte nPara = Serial1.read();
     byte trovato = 0;
     byte corretto = 0;
     int i;
@@ -63,31 +49,31 @@ void loop() {
     byte *para = (byte *)malloc(nPara * sizeof(byte));
 
     for (int j = 0; j < nPara; j++)
-      para[j] = Serial.read();
+      para[j] = Serial1.read();
 
-    if (Serial.read() == 0xBB && Serial.read() == 0xCC && Serial.read() == 0xDD)
+    if (Serial1.read() == 0xBB && Serial1.read() == 0xCC && Serial1.read() == 0xDD)
     {
       for (i = 0; i < N_AZIONI && !trovato; i++)
       {
         if (azioni[i].command == command && azioni[i].nPara == nPara)
         {
           trovato = 1;
-          Serial.print("Ho ricevuto il dato ");
-          Serial.println(command);
+          Serial1.print("Ho ricevuto il dato ");
+          Serial1.println(command);
 
-          Serial.print("N parametri = ");
-          Serial.println(nPara, DEC);
+          Serial1.print("N parametri = ");
+          Serial1.println(nPara, DEC);
 
           azioni[i].action();
 
           free(para);
         }
         else if (!trovato && i>=N_AZIONI-1)
-          Serial.println("Comando non esistente!");
+          Serial1.println("Comando non esistente!");
       }
     }
     else
-      Serial.println("Frame non corretto!");
+      Serial1.println("Frame non corretto!");
   }
 
 
