c206f4f4e816d43924b5d83bf81b43999eb8d57b
Francesco Franchina <cescus92@gmail.com>
2016-02-01 19:02:09 +0100

    Perfezionata l'app per l'apertura della porta

--- webapp/main.js
+++ webapp/main.js
@@ -1,8 +1,15 @@
-function aggiorna_img(){
-		var url = "http://192.168.2.133:81/snapshot.cgi?user=admin&pwd=&";
-		var rand=Math.random();
-		var url_unica= url+rand;
-		var img=document.getElementById("stream-img");
-		img.src=url_unica;
+function aggiorna_img() {
+    var url = "http://192.168.1.14:81/snapshot.cgi?user=admin&pwd=&";
+    var rand=Math.random();
+    var url_unica= url+rand;
+    var img=document.getElementById("stream-img");
+    img.src=url_unica;
 }
+
+function apri_porta() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://arduino.local:8080/fablab/porta/unlock', true);    
+    request.send();
+}
+
 setInterval(aggiorna_img,100);
