bd993d7a496ff6b207e8512216818f5f16e781d2
Francesco Franchina <cescus92@gmail.com>
2016-02-08 20:02:19 +0100

    Ripulito js della webapp e inseriti ip statici

--- webapp/main.js
+++ webapp/main.js
@@ -1,14 +1,18 @@
+var ip_arduino = '192.168.1.3';
+var ip_camera = '192.168.1.200';
+
+
 function aggiorna_img() {
-    var url = "http://192.168.1.14:81/snapshot.cgi?user=admin&pwd=&";
-    var rand=Math.random();
-    var url_unica= url+rand;
-    var img=document.getElementById("stream-img");
-    img.src=url_unica;
+    var url = "http://" + ip_camera + ":81/snapshot.cgi?user=admin&pwd=&";
+    var rand = Math.random();
+    var url_unica = url+rand;
+    var img = document.getElementById("stream-img");
+    img.src = url_unica;
 }
 
 function apri_porta() {
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://arduino.local:8080/fablab/porta/unlock', true);    
+    request.open('GET', "http://" + ip_arduino + ":8080/fablab/porta/unlock", true);    
     request.send();
 }
 
